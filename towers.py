6669cddbb887c5bfc341417a8d58116a7d7097c2
Vinc <vincetiu8@gmail.com>
2020-04-04 09:36:46 +0800

    Add damage
    Enemies now get damaged and killed by tower bullets. This is
    currently single-target only, with each bullet only damaging one
    enemy, even if it hits more than one. Rocket explosion types can
    be added in the future.

--- towers.py
+++ towers.py
@@ -6,20 +6,27 @@ from pathfinding import heuristic
 from heapq import *
 
 class Projectile(pg.sprite.Sprite):
-    def __init__(self, game, x, y, w, h, speed, direction):
+    def __init__(self, game, x, y, w, h, speed, direction, damage):
         self.groups = game.projectiles
         pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
         self.x = round_to_mtilesize(x) - w / 2
         self.y = round_to_mtilesize(y) - h / 2
         self.w = w
         self.h = h
         self.speed = speed
         self.direction = direction
+        self.damage = damage
 
     def update(self):
         self.x += self.speed * math.cos(self.direction)
         self.y += self.speed * math.sin(self.direction)
         self.rect = pg.Rect(self.x, self.y, self.w, self.h)
+
+        hits = pg.sprite.spritecollide(self, self.game.enemies, False)
+        if (hits):
+            hits[0].hp -= self.damage
+            self.kill()
 
 class Tower(Obstacle):
     def __init__(self, game, x, y, speed, bullet_speed, bullet_size, damage, range):
@@ -35,14 +42,12 @@ class Tower(Obstacle):
         self.search_for_enemy()
 
     def update(self):
-        print(self.current_enemy)
-
         if (pg.time.get_ticks() >= self.next_spawn and self.current_enemy != None):
             if (not self.current_enemy.alive() or heuristic((self.current_enemy.x, self.current_enemy.y), (self.x, self.y)) > self.range):
                 self.current_enemy = None
             else:
-                temp_x = round_to_mtilesize(self.current_enemy.x)
-                temp_y = round_to_mtilesize(self.current_enemy.y)
+                temp_x = self.current_enemy.x
+                temp_y = self.current_enemy.y
 
                 if (temp_x - self.x == 0):
                     if (temp_y - self.y > 0):
@@ -55,7 +60,7 @@ class Tower(Obstacle):
                     if (temp_x - self.x < 0):
                         angle += math.pi
 
-                Projectile(self.game, self.x, self.y, self.bullet_size, self.bullet_size, self.bullet_speed, angle)
+                Projectile(self.game, self.x, self.y, self.bullet_size, self.bullet_size, self.bullet_speed, angle, self.damage)
                 self.next_spawn = pg.time.get_ticks() + self.speed * 1000
 
         if (self.current_enemy == None):
