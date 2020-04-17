f2185a08392b4e1494c1b251666d3855d84c9ab5
Vinc <vincetiu8@gmail.com>
2020-04-17 09:35:13 +0800

    Add artery and vein sprites
    Adds the artery and vein sprites to the image folders. Also updates the map so they are there.

---
+++ maps/artery_and_vein.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="artery_and_vein" tilewidth="42" tileheight="42" tilecount="20" columns="5">
+ <image source="../img/tiles/body_tiles/artery_and_vein.png" width="210" height="168"/>
+</tileset>
