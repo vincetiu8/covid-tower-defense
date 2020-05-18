f3284f87e54fb6819c9adc63a7cf9954f0bb1066
Vinc <vincetiu8@gmail.com>
2020-05-18 14:14:25 +0800

    Add the m_cell into the game
    This adds the m_cell, a slow-firing tower that does lots of damage to enemies.

--- data/ui.py
+++ data/ui.py
@@ -73,9 +73,7 @@ class UI:
         
         # Draws towers
         for i, tower in enumerate(self.game.available_towers):
-            tower_img = pg.transform.scale(TOWER_DATA[tower]["stages"][0]["base_image"], self.tower_rects[i].size).convert_alpha()
-            if not TOWER_DATA[tower]["stages"][0]["area_of_effect"] and TOWER_DATA[tower]["stages"][0]["rotating"]:
-                tower_img.blit(pg.transform.scale(TOWER_DATA[tower]["stages"][0]["gun_image"], self.tower_rects[i].size), (0, 0))
+            tower_img = pg.transform.scale(TOWER_DATA[tower]["stages"][0]["image"], self.tower_rects[i].size)
             if (self.game.protein < TOWER_DATA[tower]["stages"][0]["upgrade_cost"]):
                 tower_img.fill(HALF_RED, None, pg.BLEND_RGBA_MULT)
             ui.blit(tower_img, self.tower_rects[i])
